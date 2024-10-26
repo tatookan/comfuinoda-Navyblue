8ebe78f1fde2ec003be2ca57b26f0c2b9e7f8dff
tatookan <55909686+tatookan@users.noreply.github.com>
2024-10-26 11:43:18 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .shenlanhana import AdvancedLyingSigmaSamplerNode
+
+# 将节点映射到NODE_CLASS_MAPPINGS和NODE_DISPLAY_NAME_MAPPINGS
+NODE_CLASS_MAPPINGS = {
+    "AdvancedLyingSigmaSampler": AdvancedLyingSigmaSamplerNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AdvancedLyingSigmaSampler": "Advanced Lying Sigma Sampler"
+}
